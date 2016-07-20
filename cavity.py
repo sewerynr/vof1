e9fff7ab603236427ef94aefe3bad38db85fca85
srek <sewerynrek@gmail.com>
2016-07-20 16:40:01 +0200

    simple

--- cavity.py
+++ cavity.py
@@ -21,6 +21,7 @@ nt = (tk - tp)/dt
 
 x0, y0, dl = (0, 0, 0)
 
+viscosity = 0.01
 
 import time
 
@@ -31,30 +32,12 @@ node_c, cells, bound = siatka_regularna_prost(n, dx, dy, x0, y0)
 mesh = Mesh(node_c, cells, bound)                         # 1. tworzy obiekt mesh klasy Mesh, 2. generujemy siatke dla tego obiektu funkcja siatka_reg...
 
 
-Ux = SurfField(mesh)                                       # tworzy obiekt klasy SurfField, pobierajacy obirkt mesh klasy Mesh ( na tej siatce ma tworzyc i przechowywac rozwiazanie (wartosci))
-Uy = SurfField(mesh)
-p = SurfField(mesh)
-
-#print mesh.cell_centers
-
-
-#T.setBoundaryCondition(Neuman(mesh, 0, 0))               # zero odpowiada zerowej krawedzi pobiera obiekt klasy Dirichlet (wywoluje go i tworzy)
-Ux.setBoundaryCondition(Dirichlet(mesh, 0, 0))
-Uy.setBoundaryCondition(Dirichlet(mesh, 0, 0))
-
-Ux.setBoundaryCondition(Dirichlet(mesh, 1, 0))
-Uy.setBoundaryCondition(Dirichlet(mesh, 1, 0))
+Ux = SurfField(mesh, Dirichlet)                                       # tworzy obiekt klasy SurfField, pobierajacy obirkt mesh klasy Mesh ( na tej siatce ma tworzyc i przechowywac rozwiazanie (wartosci))
+Uy = SurfField(mesh, Dirichlet)
+p = SurfField(mesh, Neuman)
 
 Ux.setBoundaryCondition(Dirichlet(mesh, 2, 1))
-Uy.setBoundaryCondition(Dirichlet(mesh, 2, 0))
 
-Ux.setBoundaryCondition(Dirichlet(mesh, 3, 0))
-Uy.setBoundaryCondition(Dirichlet(mesh, 3, 0))
-
-p.setBoundaryCondition(Neuman(mesh, 0, 0))
-p.setBoundaryCondition(Neuman(mesh, 1, 0))
-p.setBoundaryCondition(Dirichlet(mesh, 2, 0))
-p.setBoundaryCondition(Neuman(mesh, 3, 0))
 
 np.set_printoptions(precision=3)
 
@@ -62,12 +45,8 @@ np.set_printoptions(precision=3)
 from fvMatrix import fvMatrix
 einterp = EdgeField.interp
 
-Mxd, Fxd = laplace(Ux, fvMatrix)
-Myd, Fyd = laplace(Uy, fvMatrix)
-
-
-viscosity = 0.001
-
+Mxd, Fxd = laplace(viscosity, Ux)
+Myd, Fyd = laplace(viscosity, Uy)
 
 # momX_M.relax(0.7)
 # momY_M.relax(0.7)
@@ -76,22 +55,26 @@ viscosity = 0.001
 from scipy.sparse.linalg.isolve.iterative import bicgstab
 
 
+gradP = grad(p)
 
-for i in range(10):
+for i in range(3):
     print "iter", i
 
     edgeU = EdgeField.vector(einterp(Ux), einterp(Uy))  # pole wektorowe predkosci [Ux, Uy] wyinterpolowanych wartosci na krawedzie (ze sr komurek  EdgeField.interp)
     phi = edgeU.dot(mesh.Se)  # phi = v n A  gdzie An tu rowne jest dl_krawedzi obruconej o 90 stopni
-    gradP = grad(p)
 
-    Mxc, Fxc = div(phi, Ux, fvMatrix)  # ukladanie macierzy i wektora prawych stron, dostaje D i Rhs z div
-    Myc, Fyc = div(phi, Uy, fvMatrix)
+    Mxc, Fxc = div(phi, Ux)  # ukladanie macierzy i wektora prawych stron, dostaje D i Rhs z div
+    Myc, Fyc = div(phi, Uy)
 
     momX_M = Mxc - Mxd * viscosity
     momY_M = Myc - Myd * viscosity
 
-    momX_F = Fxc - Fxd * viscosity - gradP[:, 0]
-    momY_F = Fyc - Fyd * viscosity - gradP[:, 1]
+    momX_F = Fxc - Fxd * viscosity - gradP[:, 0]*mesh.cell_area
+    momY_F = Fyc - Fyd * viscosity - gradP[:, 1]*mesh.cell_area
+
+    # momX_M.relax(0.3)
+    # momY_M.relax(0.3)
+
 
     Ux.data = bicgstab(A=momX_M.sparse, b=momX_F, x0=Ux.data)[0]
     Uy.data = bicgstab(A=momY_M.sparse, b=momY_F, x0=Uy.data)[0]
@@ -107,13 +90,11 @@ for i in range(10):
     # edgeU = EdgeField.vector(einterp(Ux), einterp(Uy))  # pole wektorowe predkosci [Ux, Uy] wyinterpolowanych wartosci na krawedzie (ze sr komurek  EdgeField.interp)
     # phi = edgeU.dot(mesh.Se)  # phi = v n A  gdzie An tu rowne jest dl_krawedzi obruconej o 90 stopni
     #
-    # Mpd, Fpd = laplace(p, fvMatrix)
-    #
-    # Mpd.mulRowWise(1./A)
-    # Fpd = Fpd/A + edgeDiv(phi)/mesh.cell_area
+    # Mpd, Fpd = laplace(1./A, p)
+    # Fpd = Fpd + edgeDiv(phi)
     #
     # pres = bicgstab(A=Mpd.sparse, b=Fpd, x0=p.data)[0]
-    # p.data = p.data * 0.7 + 0.3 * pres
+    # p.setValues( p.data * 0.7 + 0.3 * pres )
     #
     # gradP = grad(p)
     # Ux.data = Ux.data - gradP[:,0]/A
@@ -127,7 +108,11 @@ for i in range(10):
 #
 #
 
-animate_contour_plot([Ux.data.reshape((n,n))])
+# animate_contour_plot([Ux.data.reshape((n,n))])
+
+Umag = np.sqrt(np.multiply(Ux.data, Ux.data) + np.multiply(Uy.data, Uy.data))
+
+animate_contour_plot([inter(mesh.xy, mesh.cells, Umag).reshape((n+1,n+1))], skip=10, repeat=False, interval=75)
 plt.show()
 # animate_contour_plot([p.data.reshape((n,n))])
 # plt.show()
